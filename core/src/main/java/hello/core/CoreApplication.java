25a64e11e590fde0bb3513a4fcba1c873d72cd67
jeongdaeun98 <uneap@sk.com>
2021-06-01 22:31:52 +0900

    새로운 할인 정책 적용(DIP, OCP) + 스프링 컨테이너 사용

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,5 +1,11 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

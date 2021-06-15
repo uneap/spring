1f039d7fc7ab8b1fb700fa597b29e533f72f9a4b
jeongdaeun98 <uneap@sk.com>
2021-06-15 22:01:07 +0900

    @Configuration & CGLIB

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -12,21 +12,33 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//call AppConfig.memberService
+//call AppConfig.memberRepository
+//call AppConfig.memberRepository
+//call AppConfig.orderService
+//call AppConfig.memberRepository
+
+//call AppConfig.memberService
+//call AppConfig.memberRepository
+//call AppConfig.orderService
 @Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 

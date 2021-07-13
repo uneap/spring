f8892db50b7b833b2959283281fbe0f5c6e166ee
jeongdaeun98 <uneap@sk.com>
2021-07-13 22:23:22 +0900

    add MyLogger - request scope

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,16 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-// AppConfig와의 충돌을 막기 위한 설정
-@ComponentScan(
-    basePackages = "hello.core.member",
-    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
-@Configuration
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }
